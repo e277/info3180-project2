e2f1bf0809ec34b04b96df83e68dc16fd01397ec
Mia Hill <119606230+miaalana@users.noreply.github.com>
2024-04-13 11:56:42 -0500

    Add files via upload

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,19 +1,86 @@
 <script setup>
 import { ref } from "vue";
 
-let message = ref("Hello World! This is a VueJS and Flask Starter Template.")
-
 </script>
 
 <template>
-    <div class="container">
-      <div class="text-center">
-        <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
-        <h1>{{ message }}</h1>
-      </div>
+    <div class="homesquares">
+        <img src="uploads/wallpaper.jpeg" alt="Photogram Logo" width="30" height="30" class="home1">
+        <div class = "information">
+            <span class="navbar-brand"><img src="uploads/cam.png" alt="Photogram Logo" width="30" height="30" class="d-inline-block align-top">Photogram</span>
+            <hr>
+            <p>Share photos of your favourite moments with friends, family and the world.</p>
+            <div class ="options">
+                <button>Register</button>
+                <button>Login</button>
+            </div>
+        </div>
     </div>
 </template>
 
 <style>
-/* Add any component specific styles here */
+.homesquares{
+        display:flex;
+        flex-direction:row;
+        margin:60px;
+        padding:30px;
+    }
+    
+    .home1{
+        width:530px;
+        height:360px;
+        border: 1px solid grey;
+        margin:20px;
+        box-shadow: 10px 10px 5px #aaaaaa;
+        border-radius: 10px;
+    }
+    
+    .information{
+        width:500px;
+        height:300px;
+        margin:20px;
+        padding:30px;
+        border: 1px solid grey;
+        box-shadow: 10px 10px 5px #aaaaaa;
+        border-radius: 10px;
+        background-color:#FFF8EB;
+    }
+    
+    .navbar-brand {
+        display: flex;
+        align-items: center;
+        justify-content: center;
+    }
+    
+    .options{
+        padding:20px;
+        margin:20px;
+    }
+    
+    button{
+        width:190px;
+        height:40px;
+    }
+    
+    .reg{
+        background-color:#049C12;
+        margin:10px;
+        border-radius: 5px;
+        color:white;
+        font-size:20px;
+        font-family: "Verdana", "Courier New", monospace;
+    }
+    
+    .log{
+        background-color:#9c048e;
+        border-radius: 5px;
+        color:white;
+        font-size:20px;
+        font-family: "Verdana", "Courier New", monospace;
+    }
+    
+    p{
+        font-size:18px;
+        font-family: "Verdana", "Courier New", monospace;
+    }
 </style>
