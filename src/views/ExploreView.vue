8cae051cbc3583abe0e74c907daeb1aaaf9e0a60
Briony Frater <briefrater@gmail.com>
2024-04-24 08:52:22 -0500

    done new button, all done on explore except like button

--- src/views/ExploreView.vue
+++ src/views/ExploreView.vue
@@ -1,6 +1,7 @@
 <script setup>
 import { ref, onMounted } from "vue";
 import FeedPost from "../components/FeedPost.vue";
+import { Plus } from 'lucide-vue-next';
 
 let allPosts = ref([]);
 
@@ -31,6 +32,14 @@ function fetchPosts() {
     <div class="pageWrapper">
         
         <div class="ctn">
+
+            <div class="addNew">
+                <a href="/post/create">
+                    <Plus size="24" stroke-width="2.75" />
+                    New Post
+                </a>
+            </div>
+
             <FeedPost v-for="post in allPosts" :key="post.user_id"
                 :userId="post.user_id"
                 :profilePic="post.profile_pic"
@@ -41,6 +50,8 @@ function fetchPosts() {
                 :likes="post.likes"
                 :isLiked="post.isLiked"
             />
+
+
         </div>
     </div>
 </template>
@@ -54,6 +65,38 @@ function fetchPosts() {
     align-items: center;
     background-color: #f9faf9;
     
+}
+
+.ctn{
+    position: relative;
+}
+
+.addNew{
+    color: #fff;
+    background-color: #1a1a1a;
+    max-width: fit-content;
+    border-radius: 100px;
+    text-transform: uppercase;
+    font-weight: 800;
+    font-size: 0.75rem;
+    position: fixed;
+    top: 125px;
+    right: 216px;
+
+   
+}
+
+.addNew:hover{
+    background-color: #0f6cf6;
+    cursor: pointer; 
+}
+
+.addNew a{
+    all: unset;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    padding: 0.4rem 1rem;
 }
 
 </style>
